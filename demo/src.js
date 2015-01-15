e2e4b7936fb53418fcf74b71765d26b01e10d190
Florian Markert <florian@scienceflow.de>
2015-01-15 14:51:09 +0100

    fft example

--- demo/src.js
+++ demo/src.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
   var wfCanvas = document.getElementById('wfctx');
   var teCanvas = document.getElementById('tectx');
   var cnt;
-  var w = 400,
-    h = 200;
+  var w = 256,
+    h = 256;
 
   var ctx = createWebglContext(lineCanvas, {
     types: ['webgl', 'experimental-webgl'],
@@ -88,8 +88,30 @@ $(document).ready(function () {
     pos: [0.0, 0.0, 0.0]
   };
   teRenderer.setTextureSize(256, 256);
+  var fft = new Fft(512);
+  var frequency = 512 * Math.random();
+  var frequency2 = 512 * Math.random();
+
+  var fftArray = [];
 
   var run = function () {
+    fftArray.length = 0;
+    for (cnt = 0; cnt < 512; cnt++) {
+      fftArray.push(Math.sin(frequency * Math.PI * (phaseShift + (cnt) / (512))) + Math.sin(frequency2 * Math.PI * (phaseShift + (cnt) / (512))));
+    }
+    var res = fft.forward(fftArray, 'hanning');
+    res[0] = 0;
+    res[511] = 0;
+    var db = fft.magToDb(fft.magnitude(res));
+    var max = -Number.MAX_VALUE;
+    for (cnt = 0; cnt < 512; cnt++) {
+      if (db[cnt] > max) {
+        max = db[cnt];
+      }
+    }
+    for (cnt = 0; cnt < 512; cnt++) {
+      db[cnt] -= max;
+    }
     var time = getTime();
     textStyle.pos = [-0.2, 0.0, 0.0];
     teRenderer.writeText('ms per Fame', textStyle);
@@ -112,10 +134,10 @@ $(document).ready(function () {
     hmRenderer.age(0.015);
     var i = 0.5;
 
-    var yPrev = (Math.sin(2 * Math.PI * (phaseShift + (0) / (w)) + Math.PI) * w / 2 + w / 2) / 2;
+    var yPrev = (Math.sin(2 * Math.PI * (phaseShift + (0) / (w)) + Math.PI) * w / 2 + w / 2);
     for (cnt = 1; cnt < w; cnt++) {
       var x = cnt;
-      var y = (Math.sin(2 * Math.PI * (phaseShift + (cnt) / (w)) + Math.PI) * w / 2 + w / 2) / 2;
+      var y = (Math.sin(2 * Math.PI * (phaseShift + (cnt) / (w)) + Math.PI) * w / 2 + w / 2);
       var yDiff = y - yPrev;
       for (var j = 0; j <= Math.ceil(Math.abs(yDiff)); j++) {
         if (yDiff >= 0) {
@@ -131,15 +153,19 @@ $(document).ready(function () {
     // render waterfall
     var d = [];
     for (cnt = 0; cnt < h; cnt++) {
-      d[cnt] = ampl * (Math.sin(Math.PI + 2 * Math.PI * (phaseDrift + (cnt) / (h))) + 1) / 2;
+      d[cnt] = (180 + db[cnt]) / 180;
     }
     var sign = Math.random();
     if (sign > 0.5) {
       phaseDrift += Math.random() / 500 * Math.PI;
       ampl += (Math.random() / 50);
+      frequency = (frequency - 1) % 512;
+      frequency2 = (frequency2 - 0.5) % 512;
     } else {
       phaseDrift -= Math.random() / 500 * Math.PI;
       ampl -= (Math.random() / 50);
+      frequency = (frequency + 0.5) % 512;
+      frequency2 = (frequency2 + 1) % 512;
     }
     if (ampl > 1) {
       ampl = 1;
