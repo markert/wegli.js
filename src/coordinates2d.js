8fc2b8abbc09e18a97a6693739f299b75d9b3916
Florian Markert <florian@scienceflow.de>
2018-11-23 15:21:06 +0100

    add text to axis

--- src/coordinates2d.js
+++ src/coordinates2d.js
@@ -5,13 +5,23 @@
   var coordinates2d = function (ctx, params) {
     var coordinatePosition = {
       lineRenderer: null,
+      textRenderer: null,
+      textStyle: {
+        font: '12px/24px Arial, serif',
+        fill: '#000000',
+        stroke: '#000000',
+        plane: 'n',
+        pos: [-1.0, -1.0, 0.0]
+      },
       corners: [1, -1, 0, -1, -1, 0, -1, 1, 0],
       colors: [0, 0, 0, 0, 0, 0, 0, 0, 0],
       backgroundCorners: [1, -1, 0, -1, -1, 0, -1, 1, 0, 1, 1, 0],
       backgroundColor: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      xTickDist: 0,
       xTicks: [],
       xTicksColor: [],
       xTickLength: 0.1,
+      yTickDist: 0,
       yTicks: [],
       yTicksColor: [],
       yTickLength: 0.1,
@@ -29,6 +39,8 @@
       }
     }
     coordinatePosition.lineRenderer = attachPloygonShader(ctx);
+    coordinatePosition.textRenderer = attachTextureShader(ctx);
+    coordinatePosition.textRenderer.setTextureSize(256, 256);
     var setVertex = function (array, position) {
       for (var i = 0; i < 3; i++)
         array.push(position[i]);
@@ -59,7 +71,8 @@
           if (params.x.tickLength) {
             coordinatePosition.xTickLength = params.x.tickLength;
           }
-          var dist = (-coordinatePosition.yAxis + 1) / (params.x.ticks + 1);
+          coordinatePosition.xTickDist = (-coordinatePosition.yAxis + 1) / (params.x.ticks + 1);
+          var dist = coordinatePosition.xTickDist;
           for (var i = 0; i < params.x.ticks; i++) {
             setVertex(coordinatePosition.xTicks, [dist * (i + 1) + coordinatePosition.yAxis, coordinatePosition.xAxis + coordinatePosition.xTickLength, 0]);
             setVertex(coordinatePosition.xTicksColor, [0, 0, 0]);
@@ -83,6 +96,8 @@
             coordinatePosition.yTickLength = params.y.tickLength;
           }
           var dist = (-coordinatePosition.xAxis + 1) / (params.y.ticks + 1);
+          coordinatePosition.yTickDist = (-coordinatePosition.xAxis + 1) / (params.y.ticks + 1);
+          var dist = coordinatePosition.yTickDist;
           for (var i = 0; i < params.y.ticks; i++) {
             setVertex(coordinatePosition.yTicks, [coordinatePosition.yAxis + coordinatePosition.yTickLength, dist * (i + 1) + coordinatePosition.xAxis, 0]);
             setVertex(coordinatePosition.yTicksColor, [0, 0, 0]);
@@ -164,11 +179,24 @@
           coordinatePosition.lineRenderer.draw(coordinatePosition.backgroundCorners, coordinatePosition.backgroundColor, 'TRIANGLE_FAN');
         }
         coordinatePosition.lineRenderer.draw(coordinatePosition.corners, coordinatePosition.colors, 'LINE_STRIP');
-        if (params.xTicks !== 'undefined') {
+        if (params.x.ticks) {
           coordinatePosition.lineRenderer.draw(coordinatePosition.xTicks, coordinatePosition.xTicksColor, 'LINES');
+          for (var i = 0; i < params.x.ticks; i++) {
+            var xPos = coordinatePosition.xTickDist * (i + 1) + coordinatePosition.yAxis;
+            var text = ((coordinatePosition.data.xMinMax[1] - coordinatePosition.data.xMinMax[0]) / (params.x.ticks + 1) * (i + 1) + coordinatePosition.data.xMinMax[0]).toFixed(2).toString();
+            coordinatePosition.textStyle.pos = [xPos - 0.1, -1, 0];
+            coordinatePosition.textRenderer.writeText(text, coordinatePosition.textStyle);
+          }
         }
-        if (params.yTicks !== 'undefined') {
+        if (params.y.ticks) {
           coordinatePosition.lineRenderer.draw(coordinatePosition.yTicks, coordinatePosition.yTicksColor, 'LINES');
+          for (var i = 0; i < params.y.ticks; i++) {
+            var yPos = coordinatePosition.yTickDist * (i + 1) + coordinatePosition.xAxis;
+            var text = ((coordinatePosition.data.yMinMax[1] - coordinatePosition.data.yMinMax[0]) / (params.y.ticks + 1) * (i + 1) + coordinatePosition.data.yMinMax[0]).toFixed(2).toString();
+            coordinatePosition.textStyle.pos = [-1, yPos - 0.05, 0];
+
+            coordinatePosition.textRenderer.writeText(text, coordinatePosition.textStyle);
+          }
         }
         for (var i = 0; i < coordinatePosition.data.values.length; i++) {
           coordinatePosition.lineRenderer.draw(coordinatePosition.data.values[i].position, coordinatePosition.data.values[i].color, 'LINE_STRIP');
